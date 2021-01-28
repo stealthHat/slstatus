c34ab71fe0c25616666b9a8ea5aae8c3b48fd54b
stealthHat <guilherme.santos@deliverymuch.com.br>
2021-01-28 08:48:35 -0300

    minor ajusts

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime,      " [%s]",           "%a %b %d %R %p " },
+	{ datetime,      " [%s]",           "%a %b %d %R%p" },
 	{ run_command,   " [s%]",   "volume" },
 	{ cpu_perc,      " [CPU  %s%%]", NULL },
 	{ run_command,      " [%s]",  "memory" },
